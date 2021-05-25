b4371dfde02a54cc25f02a95f6a9d20d7a891fcc
Michał Szczepanowski gr 3 <m.szczepanowski11@gmail.com>
2021-05-25 23:27:51 +0200

    Added filter, view selector

--- src/components/Entities/Entities.tsx
+++ src/components/Entities/Entities.tsx
@@ -1,7 +1,111 @@
-import React,{FC} from 'react';
+import React,{FC,useState} from 'react';
+import _ from 'lodash'
+import EntitiesData from './EntitiesData'
+import {Button,Message,Card,Dropdown,Input,Icon} from 'semantic-ui-react'
+import styled from 'styled-components'
+import './styles.scss'
 
-export const Entities : FC = () => {
+
+const EntitiesContent = styled.div`
+    position:absolute;
+    top:10%;
+    right:3%;
+    width:1000px;
+`
+
+const EntitiesTopBar = styled.div`
+    
+`
+
+const EntitiesRightSide = styled.div`
+   
+`
+
+const EntitiesLeftSide = styled.div`
+  
+`
+
+export const Entities:FC = () => {
+
+    const [searchTerm,setSearchTerm] = useState("")
+    const [active,setActive] = useState(1)
+    const [copied, setCopied] = useState(false);
+    
+    const setView = active => {
+        setActive(active)
+    }
+
+    const shareURL = () => {
+        const el = document.createElement('input')
+        el.value = window.location.href
+        document.body.appendChild(el)
+        el.select()
+        document.execCommand('copy')
+        document.body.removeChild(el)
+        setCopied(true)
+    }
+
     return(
-        <h1>Entities</h1>
+        <EntitiesContent>
+            <EntitiesTopBar>
+                <EntitiesLeftSide>
+                    <Button icon><Icon name='sort alphabet down'/> Sort</Button>
+                    <Button icon><Icon name='filter'/> Filters</Button>
+                    <Button icon='sort'/>
+                    <Button 
+                        icon 
+                        onClick={shareURL}>
+                        <Icon name='share'/>{' '}
+                        {!copied ? 'Share' : 'Copied!'}
+                    </Button>
+                </EntitiesLeftSide>
+            <EntitiesRightSide>
+                <Button.Group style={{marginRight:'10px'}} className='mosaic'>
+                    <Button 
+                        color='blue' 
+                        onClick={()=> setView(1)}>
+                        <Icon name='th'/>Mosaic
+                    </Button>
+                    <Button 
+                        icon 
+                        onClick={()=> setView(2)}>
+                        <Icon name='align justify' />
+                    </Button>
+                </Button.Group>
+                <Input 
+                 icon='search'
+                 placeholder='Search...'
+                 onChange={event => {setSearchTerm(event.target.value)}}
+                />
+            <Dropdown clasName='dropdown' text="Followed"/>
+            </EntitiesRightSide>
+         </EntitiesTopBar>
+
+            {EntitiesData.filter((value) => {
+               if(searchTerm === ""){
+                   return value.header
+               } else if(value.header.toLowerCase().includes(searchTerm.toLowerCase())){
+                   return value.header
+               }
+            })
+            .map((value) => {
+                return(
+                    <>
+                        {active === 1  ? <Card.Group itemsPerRow={3}>
+                            {_.times(3,() => (
+                                <Card><Message {...value}/></Card>   
+                            ))} 
+                        </Card.Group>
+                        :
+                        <Card.Group itemsPerRow={1}>
+                            {_.times(3,() => (
+                                <Card><Message {...value}/></Card>   
+                            ))}
+                        </Card.Group>}
+                    </>
+                 )
+            })}
+        </EntitiesContent>
     )
 }
+
---
+++ src/components/Entities/EntitiesData.tsx
@@ -0,0 +1,33 @@
+// <!-- <a href='https://pl.freepik.com/wektory/logo'>Logo plik wektorowy utworzone przez vectortwins - pl.freepik.com</a> -->
+const EntitiesData = [
+    {
+        header:'World Company SAS',
+        content: 'Caracas 1050,Distrito Capital,Venezuela',
+        icon:'law', 
+    },
+    {
+        header:'Subsid Corp Ltd',
+        content: '1592  Blair Court,Missouri,Bunceton',
+        icon:'globe',
+    },
+    {
+        header:'Monahan LLC	',
+        content: '756  Jones Avenue,North Carolina,Southmont',
+        icon:'gavel'
+    },
+    {
+        header:'Walker, Shields and Johns',
+        content: 'Jenaer Strasse 65,Nordrhein-Westfalen,Duisburg',
+        icon:'usd'
+    },
+    {
+        header:'Cremin and Sons',
+        content: '2231  Finwood Road,New Jersey,New Brunswick',
+        icon:'law'
+    },
+]
+
+
+
+export default EntitiesData
+
