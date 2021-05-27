1cb1a2d62b0e898a11646bef089a41688d8be26f
Michał Szczepanowski gr 3 <m.szczepanowski11@gmail.com>
2021-05-27 19:50:36 +0200

    Workspaces finished

--- src/components/common/Workspaces/SingleWorkspace.tsx
+++ src/components/common/Workspaces/SingleWorkspace.tsx
@@ -1,44 +1,45 @@
 import React from 'react';
-import { Card, Image } from 'semantic-ui-react'
+import { Card, Image,Icon } from 'semantic-ui-react'
 import styled from 'styled-components'
 import 'semantic-ui-css/semantic.min.css'
 
+const HeaderImage = styled.div`
+position:relative;
+bottom:40px;
+left:5%;
+background-color:white;
+border-radius:2px;
+padding:10px;
+float:left;
+box-shadow: 0px 0px 3px 0px rgba(0,0,0,0.75);
+
+`
+const SingleWorkspaceContent = styled.div`
+    margin-right:15px;
+`
 
 const SingleWorkspace = (props) => {
    
-    const HeaderImage = styled.div`
-        position:relative;
-        bottom:15%;
-        left:5%;
-        width:auto;
-        background-color:white;
-        border-radius:2px;
-        display:inline-block;
-        padding:5px;
-        float:left;
-        box-shadow: 0px 0px 3px 0px rgba(0,0,0,0.75);
-
-    `
-
-
     return(
-        
-            <Card link style={{display:'inline-block'}} fluid>
-                <Image src={props.workspace.image} />
+        <SingleWorkspaceContent>
+            <Card link style={{display:'inline-block',margin:'10px'}}>
+                <Image style={{width:'280px',height:'140px'}} src={props.workspace.image} />
                 <HeaderImage>
-                        <img  src={props.workspace.headerImage} alt="single_workspace_icon"></img>
+                        <Icon  size='huge' name={props.workspace.headerImage} alt="single_workspace_icon"/>
                     </HeaderImage>
                 <Card.Content extra>
-                    <Card.Header style={{fontSize:'20px',textAlign:'right'}}>{props.workspace.header}
+                    <Card.Header style={{fontSize:'15px',textAlign:'right'}}>{props.workspace.header}
                     </Card.Header>
                     <Image src={props.workspace.icon} />
-                    <Card.Meta style={{textAlign:'left'}}>
+                    <Card.Meta style={{position:'absolute',float:'left',marginRight:'10px'}}>
+                       <Icon name={props.workspace.metaIcon}/>
                         {props.workspace.meta}  
                         <Card.Content style={{fontSize:'12px'}} extra>{props.workspace.extra} </Card.Content>
                     </Card.Meta>
                 </Card.Content>
             </Card>
-    )
+         </SingleWorkspaceContent>
+     )
 }
   
 export default SingleWorkspace
--- src/components/common/Workspaces/Workspaces.tsx
+++ src/components/common/Workspaces/Workspaces.tsx
@@ -5,64 +5,86 @@ import {Card} from 'semantic-ui-react'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import 'semantic-ui-css/semantic.min.css'
 
 
-const Content = styled.div`
+const WorkspacesContent = styled.div`
     position:absolute;
     top:60%;
-    max-width:1100px;
-    max-height:250px;
-    margin:auto;
-    left:20%;
+    width:1120px;
+    height:400px;
+    right:3%;
 `
+const settings = {
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+};
 
 export default class Workspaces extends React.Component {
-   
 
     render() {
-     
-     return(
-       <Content>
+        
+     return (
+       <WorkspacesContent>
            <header style={{fontSize:'30px',margin:'20px'}}>Workspaces</header>
-              <Card.Group itemsPerRow='4'>
-                <SingleWorkspace
+           <Slider {...settings}>
+             <SingleWorkspace
                     workspace={{
-                        image:'https://picsum.photos/700/400',
+                        image:'./icons/client_contract.jpeg',
                         header:'Client Contract',
-                        headerImage:'./icons/contract3.png',
+                        headerImage:'edit outline',
                         meta:'Contract ・ 150 users',
+                        metaIcon:'edit outline',
+                        extra:'Last updated 2 days ago'
+                    }}
+                />
+             <SingleWorkspace
+                    workspace={{
+                        image:'./icons/client_contract.jpeg',
+                        header:'Supplier Contract',
+                        headerImage:'edit outline',
+                        meta:'Contract ・ 25 users',
+                        metaIcon:'edit outline',
+                        extra:'Last updated 2 days ago'
+                    }}
+                />
+                 <SingleWorkspace
+                    workspace={{
+                        image:'./icons/corporate.jpeg',
+                        header:'Corporate',
+                        headerImage:'building',
+                        meta:'Corporate ・ 25 users',
+                        metaIcon:'building',
+                        extra:'Last updated 2 days ago'
+                    }}
+                />
+                 <SingleWorkspace
+                    workspace={{
+                        image:'./icons/skyscraper.jpeg',
+                        header:'Group norms',
+                        headerImage:'book',
+                        meta:'Norms ・ 25 users',
+                        metaIcon:'book',
                         extra:'Last updated 2 days ago'
                     }}
                 />
                 <SingleWorkspace
                     workspace={{
-                        image:'https://picsum.photos/700/400',
-                        header:'Supplier contract',
-                        headerImage:'./icons/contract3.png',
+                        image:'./icons/client_contract.jpeg',
+                        header:'Real estate contract',
+                        headerImage:'edit outline',
                         meta:'Contract ・ 25 users',
+                        metaIcon:'edit outline',
                         extra:'Last updated 2 days ago'
                     }}
                 />
-                 <SingleWorkspace
-                    workspace={{
-                        image:'https://picsum.photos/700/400',
-                        header:'Corporate',
-                        headerImage:'./icons/job.png',
-                        meta:'Contract ・ 25 users',
-                        extra:'Last updated 2 days ago'
-                    }}
-                />
-                 <SingleWorkspace
-                    workspace={{
-                        image:'https://picsum.photos/700/400',
-                        header:'Group norms',
-                        headerImage:'./icons/book.png',
-                        meta:'Contract ・ 25 users',
-                        extra:'Last updated 2 days ago'
-                    }}
-                />
-              </Card.Group>
-          </Content>
+             {/* <div><img src='https://picsum.photos/200'alt="Credit to Igor Ovsyannykov on Unsplash"/></div>
+             <div><img src='https://picsum.photos/200' alt="Credit to Pierre Châtel-Innocenti on Unsplash"/></div>
+             <div><img src='https://picsum.photos/200' alt="Credit to Richard Nolan on Unsplash"/></div>
+             <div><img src='https://picsum.photos/200' alt="Credit to Cristina Gottardi on Unsplash"/></div> */}
+              </Slider>
+          </WorkspacesContent>
       )
    }
 }
