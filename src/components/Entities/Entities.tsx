322893b0529f4ec35562d9871e7364e56b78e9cc
Michał Szczepanowski gr 3 <m.szczepanowski11@gmail.com>
2021-06-01 23:07:30 +0200

    Entities finished

--- src/components/Entities/Entities.tsx
+++ src/components/Entities/Entities.tsx
@@ -1,40 +1,71 @@
+/* eslint-disable array-callback-return */
 import React,{FC,useState} from 'react';
+import {useSelector} from 'react-redux'
+import {IState} from '../../reducers/index'
+import { IPhotosReducer } from '../../reducers/photosReducer';
 import _ from 'lodash'
-import EntitiesData from './EntitiesData'
-import {Button,Message,Card,Dropdown,Input,Icon} from 'semantic-ui-react'
+import {EntitiesData} from './EntitiesData'
+import {Button,Card,Dropdown,Input,Icon,Image} from 'semantic-ui-react'
 import styled from 'styled-components'
 import './styles.scss'
 
 
 const EntitiesContent = styled.div`
     position:absolute;
-    top:10%;
-    right:3%;
-    width:1000px;
+    top:7%;
+    right:2%;
+    padding:10px;
+    background-color:white;
 `
 
 const EntitiesTopBar = styled.div`
-    
+    position:relative;
+    bottom:25px;
 `
 
 const EntitiesRightSide = styled.div`
-   
+
+   text-align:right;
 `
 
 const EntitiesLeftSide = styled.div`
-  
+   position:relative;
+   top:37px;
+   text-align:left; 
 `
 
+const followedDropdownStyle ={
+    color:'rgb(26,37,135)',
+    padding:'9px',
+    marginLeft:'15px',
+    marginRight:'2px',
+    border:'1px solid rgb(44,56,147)',
+    borderRadius:'4px'
+}
+
 export const Entities:FC = () => {
+
+    const {photosList} = useSelector<IState,IPhotosReducer>(globalState => ({
+        ...globalState.photos,
+    }));
+
+    const [width] = useState('1000px')
+    const [boxShadow] = useState('0px 0px 2px 0px rgba(0,0,0,0.75)')
+    const [border] = useState('5px')
 
     const [searchTerm,setSearchTerm] = useState("")
     const [active,setActive] = useState(1)
     const [copied, setCopied] = useState(false);
+    const [clicked,isClicked] = useState(true)
     
     const setView = active => {
         setActive(active)
     }
 
+    const setClicked = () => {
+        isClicked(!clicked)
+    }
+   
     const shareURL = () => {
         const el = document.createElement('input')
         el.value = window.location.href
@@ -44,31 +75,67 @@ export const Entities:FC = () => {
         document.body.removeChild(el)
         setCopied(true)
     }
-
     return(
-        <EntitiesContent>
+        
+        <EntitiesContent style={clicked ? {width:width,boxShadow:boxShadow,borderRadius:border} : {width:'1400px',boxShadow:'none',borderRadius:'none'}}>
+            <h2><strong>Entities </strong><Icon name='setting'/></h2>
             <EntitiesTopBar>
                 <EntitiesLeftSide>
-                    <Button icon><Icon name='sort alphabet down'/> Sort</Button>
-                    <Button icon><Icon name='filter'/> Filters</Button>
-                    <Button icon='sort'/>
-                    <Button 
-                        icon 
-                        onClick={shareURL}>
-                        <Icon name='share'/>{' '}
-                        {!copied ? 'Share' : 'Copied!'}
-                    </Button>
+                    <div className='entities-filters-content'>
+                        <div className='filter-all-content'> 
+                            <Icon name='dot circle outline'/>
+                            <Dropdown text='All'>
+                               <Dropdown.Menu>                       
+                                 <header style={{padding:'10px',color:'gray'}}>Rows ale filtered by the followig conditons starting from the top</header> 
+                                   <div className='filter-all-content-after'> 
+                                      <div>
+                                          <Icon name='cancel'/>Where
+                                          <Dropdown text='Company'/>
+                                          <Dropdown text='Contains'/>
+                                          <Input size='mini' placeholder='Type..'/>
+                                      </div>
+                                      <div>
+                                         <Icon name='cancel'/>Where
+                                         <Dropdown text='Status'/>
+                                         <Dropdown text='Is'/>
+                                         <Input size='mini' placeholder='Type..'/>
+                                         <Dropdown text='In'/>
+                                         <Input size='mini' placeholder='Entity..'/>
+                                      </div>
+                                      <div>
+                                         <Icon name='cancel'/>And
+                                         <Dropdown style={{marginLeft:'35px'}} text='Status'/>
+                                         <Dropdown text='Ends before'/>
+                                         <Input size='mini' placeholder='Date..'/>
+                                         <Dropdown text='In'/>
+                                         <Input size='mini' placeholder='Entity..'/>
+                                      </div>
+                                      <div style={{color:'rgb(44,56,147)'}}>
+                                          <Icon name='plus'/>Add filter
+                                          <Dropdown text='choose property'/>
+                                      </div>                 
+                                   </div>
+                               </Dropdown.Menu>
+                            </Dropdown>
+                        </div> 
+                        <Icon name='ellipsis horizontal'/> {' '}|
+                        <Icon fitted name='sort alphabet down'>{' '}Sort</Icon>
+                        <Icon fitted name='filter'>{' '}Filters</Icon> 
+                        <Icon onClick={setClicked} name='expand arrows alternate'/> 
+                        {console.log(clicked)}
+                    |   <Icon fitted onClick={shareURL} name='share'>{' '}{!copied ? 'Share' : 'Copied!'}</Icon>
+                    </div>
                 </EntitiesLeftSide>
             <EntitiesRightSide>
-                <Button.Group style={{marginRight:'10px'}} className='mosaic'>
-                    <Button 
-                        color='blue' 
-                        onClick={()=> setView(1)}>
+                <Button.Group className='mosaic'>
+                    <Button
+                        color='blue'
+                        onClick={() => setView(1)}>
                         <Icon name='th'/>Mosaic
                     </Button>
                     <Button 
                         icon 
-                        onClick={()=> setView(2)}>
+                        onClick={() => setView(2)}>
                         <Icon name='align justify' />
                     </Button>
                 </Button.Group>
@@ -77,9 +144,9 @@ export const Entities:FC = () => {
                  placeholder='Search...'
                  onChange={event => {setSearchTerm(event.target.value)}}
                 />
-            <Dropdown clasName='dropdown' text="Followed"/>
+            <Dropdown style={followedDropdownStyle} clasName='dropdown' text="Followed"/>
             </EntitiesRightSide>
-         </EntitiesTopBar>
+         </EntitiesTopBar> 
 
             {EntitiesData.filter((value) => {
                if(searchTerm === ""){
@@ -88,20 +155,44 @@ export const Entities:FC = () => {
                    return value.header
                }
             })
-            .map((value) => {
+            .map((value,key) => {
                 return(
                     <>
-                        {active === 1  ? <Card.Group itemsPerRow={3}>
-                            {_.times(3,() => (
-                                <Card><Message {...value}/></Card>   
-                            ))} 
-                        </Card.Group>
-                        :
-                        <Card.Group itemsPerRow={1}>
-                            {_.times(3,() => (
-                                <Card><Message {...value}/></Card>   
-                            ))}
-                        </Card.Group>}
+                        {active === 1  ? 
+                            <Card.Group itemsPerRow={3}>
+                                {_.times(6,() => (
+                                    <Card>
+                                        <Card.Content>
+                                            <Image style={{marginBottom:'0',borderRadius:'5px'}}
+                                                size='tiny'
+                                                floated='left'
+                                                src={photosList?.[key * 2]?.thumbnailUrl}
+                                            />  
+                                                <Card.Header style={{color:'rgb(44,56,137)'}}>{value.header}</Card.Header>
+                                                <span style={{display:'block',marginTop:"15px",textAlign:'left'}}>{value.description}</span>
+                                        </Card.Content>
+                                    </Card>     
+                                ))} 
+                            </Card.Group>
+                            
+                            :  
+                            
+                            <Card.Group itemsPerRow={1}>
+                                {_.times(6,() => (
+                                  <Card>
+                                    <Card.Content>
+                                        <Image style={{marginBottom:'0',borderRadius:'5px'}}
+                                            size='tiny'
+                                            floated='left'
+                                            src={photosList?.[key * 2]?.thumbnailUrl}
+                                        />  
+                                            <Card.Header style={{color:'rgb(44,56,137)'}}>{value.header}</Card.Header>
+                                            <span style={{display:'block',marginTop:"35px",textAlign:'left'}}>{value.description}</span>
+                                    </Card.Content>
+                              </Card>     
+                                ))}
+                            </Card.Group>
+                         }
                     </>
                  )
             })}
@@ -109,3 +200,4 @@ export const Entities:FC = () => {
     )
 }
 
+export default Entities
--- src/components/Entities/EntitiesData.tsx
+++ src/components/Entities/EntitiesData.tsx
@@ -1,33 +1,25 @@
-// <!-- <a href='https://pl.freepik.com/wektory/logo'>Logo plik wektorowy utworzone przez vectortwins - pl.freepik.com</a> -->
-const EntitiesData = [
+export const EntitiesData = [
     {
         header:'World Company SAS',
-        content: 'Caracas 1050,Distrito Capital,Venezuela',
-        icon:'law', 
+        description: 'Caracas 1050,Distrito Capital,Venezuela',
     },
     {
         header:'Subsid Corp Ltd',
-        content: '1592  Blair Court,Missouri,Bunceton',
-        icon:'globe',
+        description: '1592  Blair Court,Missouri,Bunceton',
     },
     {
         header:'Monahan LLC	',
-        content: '756  Jones Avenue,North Carolina,Southmont',
-        icon:'gavel'
+        description: '756  Jones Avenue,North Carolina,Southmont',
     },
     {
-        header:'Walker, Shields and Johns',
-        content: 'Jenaer Strasse 65,Nordrhein-Westfalen,Duisburg',
-        icon:'usd'
+        header:'Walker Shields',
+        description: 'Jenaer Strasse 65,Nordrhein-Westfalen,Duisburg',
+
     },
     {
         header:'Cremin and Sons',
-        content: '2231  Finwood Road,New Jersey,New Brunswick',
-        icon:'law'
+        description: '2231  Finwood Road,New Jersey,New Brunswick',
     },
 ]
 
-
-
-export default EntitiesData
 
